43745fb38c35c304648a44449a1e50e44292156d
Cristian Brochero <cbrocher@cuc.edu.co>
2022-03-02 19:53:59 -0500

    primer punto

--- Taller1_python.py
+++ Taller1_python.py
@@ -5,3 +5,15 @@ Created on Wed Mar  2 19:14:14 2022
 @author: Cristian Brochero
 """
 
+don=int( input ("donacion a ingresar $"))
+tele= don*0.2
+sis= tele*0.15
+admin= sis*0.3
+Conta= don -(tele + sis + admin)
+
+print (f"la donacion es: ${tele:,}")
+print (f"la donacion es: ${sis:,}")
+print (f"la donacion es: ${admin:,}")
+print (f"la donacion es: ${Conta:,}")
+
+
